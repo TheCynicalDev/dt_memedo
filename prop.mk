70c77a07741941b7066154dc1223c42925b5dd11
Michael Bestas <mkbestas@lineageos.org>
2020-01-04 15:40:59 +0200

    mido: Disable DPM features
    * Stops it from crashing, most devices have it disabled anyway.
    
    Change-Id: If5f9e94f99d7c22b2f057dd1785c45186029b023

--- prop.mk
+++ prop.mk
@@ -89,6 +89,10 @@ vendor.display.disable_skip_validate=1 \
 vendor.gralloc.enable_fb_ubwc=1 \
 sys.display-size=1920x1080
 
+# DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.dpm.feature=0
+
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
 drm.service.enabled=true
